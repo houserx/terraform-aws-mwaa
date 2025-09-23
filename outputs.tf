438886bf49b9fcce1fba2bbc677256110bb7c604
hrx-joseph-fahnestock <joseph.fahnestock@houserx.com>
2025-09-23 14:10:41 -0400

    fix(DATA-298): attribute name typo (#3)

--- outputs.tf
+++ outputs.tf
@@ -45,26 +45,26 @@ output "aws_s3_bucket_name" {
 
 output "mwaa_dag_processing_cloudwatch_log_group_arn" {
   description = "CloudWatch Log Group ARN for Apache Airflow DAG Processing Logs"
-  value       = try(aws_mwaa_environment.mwaa.logging_configuration[0].dag_processing_logs[0].cloudwatch_log_group_arn, null)
+  value       = try(aws_mwaa_environment.mwaa.logging_configuration[0].dag_processing_logs[0].cloud_watch_log_group_arn, null)
 }
 
 output "mwaa_scheduler_cloudwatch_log_group_arn" {
   description = "CloudWatch Log Group ARN for Apache Airflow Scheduler Logs"
-  value       = try(aws_mwaa_environment.mwaa.logging_configuration[0].scheduler_logs[0].cloudwatch_log_group_arn, null)
+  value       = try(aws_mwaa_environment.mwaa.logging_configuration[0].scheduler_logs[0].cloud_watch_log_group_arn, null)
 }
 
 output "mwaa_task_log_group_arn" {
   description = "CloudWatch Log Group ARN for Apache Airflow Task Logs"
-  value       = try(aws_mwaa_environment.mwaa.logging_configuration[0].task_logs[0].cloudwatch_log_group_arn, null)
+  value       = try(aws_mwaa_environment.mwaa.logging_configuration[0].task_logs[0].cloud_watch_log_group_arn, null)
 }
 
 output "mwaa_webserver_log_group_arn" {
   description = "CloudWatch Log Group ARN for Apache Airflow Webserver Logs"
-  value       = try(aws_mwaa_environment.mwaa.logging_configuration[0].webserver_logs[0].cloudwatch_log_group_arn, null)
+  value       = try(aws_mwaa_environment.mwaa.logging_configuration[0].webserver_logs[0].cloud_watch_log_group_arn, null)
 }
 
 output "mwaa_worker_log_group_arn" {
   description = "CloudWatch Log Group ARN for Apache Airflow Worker Logs"
-  value       = try(aws_mwaa_environment.mwaa.logging_configuration[0].worker_logs[0].cloudwatch_log_group_arn, null)
+  value       = try(aws_mwaa_environment.mwaa.logging_configuration[0].worker_logs[0].cloud_watch_log_group_arn, null)
 }
 
